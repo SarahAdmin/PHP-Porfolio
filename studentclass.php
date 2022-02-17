03784f9225cb43cd9127ba6a432e03f6fd2e81de
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2022-02-17 10:10:56 +0000

    Create studentclass.php

---
+++ studentclass.php
@@ -0,0 +1,22 @@
+<?php 
+class Student() {
+  public $id; 
+  public $firstname;
+  public $lastname;
+  public $emailaddres;
+
+  function _setid($id) {
+    $this->id = $id;
+  }
+  function _setfirstname($firstname) {
+    $this->firstname= $firstname; 
+  }
+  function _setlastname($lastname) {
+    $this->lastname = $lastname; 
+  }
+  function _setemailaddress($emailaddress) {
+    $this->emailaddress = $emailaddress; 
+  }
+}
+
+?>
